98c4c20c01d707d0b0636fb0683162650d66f562
James Abbott <j.abbott@dundee.ac.uk>
2026-03-01 18:50:58 +0000

    Add beginning of workflow, copied from previous example with many rules currently commented out

---
+++ bin/run_workflow.sh
@@ -0,0 +1,26 @@
+#!/bin/env bash
+
+# Runs a Snakemake workflow to generate assemblies from raw data within data directory
+
+set -e 
+
+# Some sanity checking...
+# work out where we are running from, taking symlinks into account
+run_dir=$(readlink -f "$0")
+analysis_root=$(dirname ${run_dir}|sed 's/\/bin//')
+
+# now work out where we are, taking symlinks into account
+#'curr_dir=$(pwd|sed 's/\/analysis\/[A-Za-z0-9]*$//')
+curr_dir=$(pwd)
+real_base_dir=$(readlink -f $curr_dir)
+if [[ ! -z "${real_base_dir}" ]]; then
+	base_dir=${real_base_dir}
+fi
+
+# Are we in the right place?
+if [[ "${base_dir}" != "${analysis_root}" ]]; then
+	echo "This script should be run from the top-level directory of the project"
+	exit 1
+fi
+
+snakemake --profile profile $@
