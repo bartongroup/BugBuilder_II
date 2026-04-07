41da0034fd985560ac5c7ceabfd9a2343f2b9ff5
James Abbott <j.abbott@dundee.ac.uk>
2026-04-07 23:19:40 +0100

    Refactored common methods to common/download.py

---
+++ workflow/scripts/common/download.py
@@ -0,0 +1,143 @@
+"""
+Common functions for downloading files for databases, including GTDB-Tk, Bakta,
+and BUSCO lineages
+ """
+
+import threading
+import requests
+from requests.adapters import HTTPAdapter, Retry
+
+def create_http_session():
+    """
+    Factory function which creates requests session 
+    
+    Required params:
+        None
+
+    Returns:
+        session(requests.Session): HTTP session with retry strategy
+    """
+
+    s = requests.Session()
+    retries = Retry(
+        total=5, 
+        backoff_factor=1, 
+        status_forcelist=[502, 503, 504]
+    )
+    adapter = HTTPAdapter(max_retries=retries)
+    s.mount("https://", adapter)
+    s.mount("http://", adapter)
+
+    return s
+
+def init_worker():
+
+    """
+    Initializes worker for multiprocessing pool using factory function 
+    
+    Required params:
+        None
+
+    Returns:
+        None
+    """
+
+    global session
+    session = create_http_session()
+
+def Handler(start, end, url, filename):
+    """
+    Handler function for multithreaded download for each part of a file
+    
+    Required params:
+        start(int): starting byte of the part to download
+        end(int): ending byte of the part to download
+        url(str): URL of the file to download
+        filename(str): name of the file to save the downloaded part
+    
+    Returns:
+        None
+    """
+    headers = {'Range': f'bytes={start}-{end}'}
+    try:
+        with create_http_session() as s:
+            print(f"Downloading bytes {start} to {end} from {url} to {filename}")
+            r = s.get(url, headers=headers, stream=True)
+
+            with open(filename, "r+b") as fp:
+                fp.seek(start)
+                for chunk in r.iter_content(chunk_size=1024 * 1024):
+                    if chunk:
+                        fp.write(chunk)
+            print(f"Finished downloading bytes {start} to {end} from {url} to {filename}")
+    except requests.exceptions.RequestException as errex:
+        print(f"Exception while downloading bytes {start} to {end} from {url} to {filename} - {errex}")
+
+def make_request(uri):
+
+    """
+    Makes HTTP request and returns result body
+
+    Required params:
+        uri(str): URI for request
+
+    Returns:
+        text of response
+    """
+
+    try:
+        with create_http_session() as s:
+            r = s.get(uri, timeout=30)
+            r.raise_for_status()
+
+            return r.text
+    except requests.exceptions.RequestException as errex:
+        print(f"Exception: {uri} - {errex}")
+
+def download_file_parallel(url_of_file, name, number_of_threads):
+    """
+    Downloads file using multiple threads
+    
+    Required params:
+        url_of_file(str): URL of file to download
+        name(str): name to save file as
+        number_of_threads(int): number of threads to use for download
+    
+    Returns:
+        None
+    """
+
+    r = requests.head(url_of_file)
+    
+    if name:
+        file_name = name
+    else:
+        file_name = url_of_file.split('/')[-1]
+
+    try:
+        file_size = int(r.headers['Content-Length'])
+    except:
+        print("Invalid URL or missing Content-Length header.")
+        return
+
+    part = file_size // number_of_threads
+    with open(file_name, "wb") as fp:
+        fp.truncate(file_size)
+
+    threads = []
+    for i in range(number_of_threads):
+        start = part * i
+        # Make sure the last part downloads till the end of file
+        end = file_size - 1 if i == number_of_threads - 1 else (start + part - 1)
+
+        t = threading.Thread(target=Handler, kwargs={
+            'start': start,
+            'end': end,
+            'url': url_of_file,
+            'filename': file_name
+        })
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
--- workflow/scripts/download_bakta_db.py
+++ workflow/scripts/download_bakta_db.py
@@ -8,80 +8,15 @@ from pathlib import Path
 from argparse import ArgumentParser
 import tarfile
 import os
+import sys
 import threading
 
 import requests
 
-def Handler(start, end, url, filename):
-    """
-    Handler function for each thread to download a part of the file
-    
-    Required params:
-        start(int): starting byte of the part to download
-        end(int): ending byte of the part to download
-        url(str): URL of the file to download
-        filename(str): name of the file to save the downloaded part
-    
-    Returns:
-        None
-    """
-    headers = {'Range': f'bytes={start}-{end}'}
-    r = requests.get(url, headers=headers, stream=True)
+script_dir = Path(__file__).parent.resolve()
+sys.path.insert(0, str(script_dir))
 
-    with open(filename, "r+b") as fp:
-        fp.seek(start)
-        for chunk in r.iter_content(chunk_size=1024 * 1024):
-            if chunk:
-                fp.write(chunk)
-
-def download_file(url_of_file, name, number_of_threads):
-    """
-    Downloads file using multiple threads
-    
-    Required params:
-        url_of_file(str): URL of file to download
-        name(str): name to save file as
-        number_of_threads(int): number of threads to use for download
-    
-    Returns:
-        None
-    """
-
-    r = requests.head(url_of_file)
-    
-    if name:
-        file_name = name
-    else:
-        file_name = url_of_file.split('/')[-1]
-
-    try:
-        file_size = int(r.headers['Content-Length'])
-    except:
-        print("Invalid URL or missing Content-Length header.")
-        return
-
-    part = file_size // number_of_threads
-    with open(file_name, "wb") as fp:
-        fp.truncate(file_size)
-
-    threads = []
-    for i in range(number_of_threads):
-        start = part * i
-        # Make sure the last part downloads till the end of file
-        end = file_size - 1 if i == number_of_threads - 1 else (start + part - 1)
-
-        t = threading.Thread(target=Handler, kwargs={
-            'start': start,
-            'end': end,
-            'url': url_of_file,
-            'filename': file_name
-        })
-        threads.append(t)
-        t.start()
-
-    for t in threads:
-        t.join()
-
+from common.download import Handler, download_file_parallel
 
 def main():
     """ Main process """
@@ -101,7 +36,7 @@ def main():
 
     local_db_file = Path(f'{database_dir}/bakta.tar.xz')
 
-    download_file(db_url, local_db_file, number_of_threads=16)
+    download_file_parallel(db_url, local_db_file, number_of_threads=16)
 
     with tarfile.open(f"{local_db_file}", "r:xz") as handle:
         handle.extractall(path=f'{database_dir}/', filter="data") 
