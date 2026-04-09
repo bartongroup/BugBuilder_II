07ca0b8a966e4056b88555acb050c784e38acee6
James Abbott <j.abbott@dundee.ac.uk>
2026-04-09 19:02:51 +0100

    Enable selection of type/version for bakta, with metadata recording

--- workflow/scripts/common/download.py
+++ workflow/scripts/common/download.py
@@ -4,12 +4,19 @@ and BUSCO lineages
  """
 
 from pathlib import Path
+import pprint
+import json
+import re
+import sys
+import inspect
+
 import tarfile
 import os
 
 import threading
 import requests
 from requests.adapters import HTTPAdapter, Retry
+import portalocker
 
 def create_http_session():
     """
@@ -98,27 +105,24 @@ def make_request(uri):
     except requests.exceptions.RequestException as errex:
         print(f"Exception: {uri} - {errex}")
 
-def download_file(download_dir, url):
+def download_file(url, name):
 
     """ 
     Downloads tar section using requests 
     
     Required parameters:
-        download_dir(Path): directory to save downloaded file in
         url(str): url of file to download
+        name(str): name to save file as
     
     Returns:
         None
     """
 
-    local_filename = Path(url.split('/')[-1])
-    local_filename = download_dir / local_filename
-
     try:
         with create_http_session() as s:
             r = s.get(url, stream=True, timeout=60) 
             r.raise_for_status()
-            with open(local_filename, 'wb') as f:
+            with open(name, 'wb') as f:
                 for chunk in r.iter_content(chunk_size=8192):
                     f.write(chunk)
     except requests.exceptions.RequestException as errex:
@@ -171,3 +175,133 @@ def download_file_parallel(url_of_file, name, number_of_threads):
 
     for t in threads:
         t.join()
+
+def update_db_version(database_dir, database, version, db_type):
+
+    """
+    Updates JSON file with database version information using locking to cope
+    with concurrent access
+
+    Required params:
+        database_dir(Path): directory where JSON file is located
+        database(str): name of database to update version for
+        version(str): version number to update in JSON file
+        db_type(str): type of database to update
+
+    Returns:
+        None
+    """
+    json_file = Path(f'{database_dir}/db_versions.json')
+
+    if json_file.exists():
+        with portalocker.Lock(f'{database_dir}/db_versions.json', 'r+', timeout=10) as version_file:
+            try:
+                versions = json.load(version_file)
+            except json.JSONDecodeError:
+                versions = {}
+
+            versions[database] = version
+
+            version_file.seek(0)
+            json.dump(versions, version_file, indent=4)
+            version_file.truncate()
+    else:
+        with portalocker.Lock(f'{database_dir}/db_versions.json', 'w', timeout=10) as version_file:
+            versions = {database: {'version': version, 'type': db_type}}
+            json.dump(versions, version_file, indent=4)
+
+
+def get_zenodo_db_version(doi, version):
+    """
+    Resolves Zenodo DOI to get record id for database version
+
+    Required params:
+        doi(str): 'Concept' (master) DOI of Zenodo record
+        version(str): version number of database to resolve (e.g. 'latest', '1.0', '2.0', etc.)
+
+    Returns:
+        db_url(str): URL of database file for specified version
+        version(str): version number of database selected
+    """
+
+    if (version != 'latest' and not re.match(r'^\d+(\.\d+)*$', version)):
+        raise ValueError(f"Version ({version}) must be 'latest' or in the format '1.0', '2.0', etc.")
+
+    record_id = doi.split('.')[-1]
+    base_url = "https://zenodo.org/api/records/"
+    uri = f"{base_url}{record_id}"
+
+    json_response = make_request(uri)
+    if json_response is not None:
+        response_data = json.loads(json_response)
+
+        # Get the Concept ID (this links all versions together)
+        concept_id = response_data.get('conceptrecid')
+    
+        # Query Zenodo for all records sharing this Concept ID
+        versions_url = f"https://zenodo.org/api/records/?q=conceptrecid:{concept_id}&all_versions=True"
+        v_response = requests.get(versions_url)
+        v_response.raise_for_status()
+        all_versions_data = v_response.json()
+        
+        versions = {}
+        for entry in all_versions_data['hits']['hits']:
+            versions[entry['metadata'].get('version')] =  {
+                "date": entry['metadata'].get('publication_date'),
+                "doi": entry['doi'],
+                "record_id": entry['id']
+            }
+        
+        # Sort by date or version number if needed
+        if version == 'latest':
+            latest_version = max(versions.items())
+            db_record_id = latest_version[1]['record_id']
+            version = latest_version[0]
+        else:
+            if version in versions:
+                db_record_id = versions[version]['record_id']
+                version = versions[version]['version']
+            else:
+                raise ValueError(f"Version ({version}) not found.")
+                sys.exit(1)
+
+        return(db_record_id, version)
+
+def get_zenodo_download_url(record_id, type):
+
+    """
+    Gets download URL for Zenodo record id
+
+    Required params:
+        record_id(str): Zenodo record id for database version
+        type(str): type of file to download (e.g. 'full' or 'light')
+        
+    Returns:
+        db_url(str): URL of database file for specified version
+    """
+
+    # Identify database name based on calling script
+    caller_frame = inspect.stack()[1]
+    caller_filename = caller_frame.filename
+
+    if 'bakta' in caller_filename:
+        if type == 'full':
+            file_name = 'db.tar.xz'
+        else:
+            file_name = 'db-light.tar.xz'
+
+    base_url = "https://zenodo.org/api/records/"
+    uri = f"{base_url}{record_id}"
+
+    json_response = make_request(uri)
+    if json_response is not None:
+        response_data = json.loads(json_response)
+        files = response_data.get('files', [])
+        if files:
+            for file in files:
+                if file.get('key') == file_name:
+                    return file.get('links', {}).get('self')
+        else:
+            raise ValueError(f"No files found for record ID {record_id}.")
+    else:
+        raise ValueError(f"Could not retrieve data for record ID {record_id}.")
--- workflow/scripts/download_bakta_db.py
+++ workflow/scripts/download_bakta_db.py
@@ -10,36 +10,56 @@ import tarfile
 import os
 import sys
 import threading
+import json
 
+import portalocker
 import requests
 
 script_dir = Path(__file__).parent.resolve()
 sys.path.insert(0, str(script_dir))
 
-from common.download import Handler, download_file_parallel
+from common.download import download_file, download_file_parallel, update_db_version, get_zenodo_download_url, get_zenodo_db_version
 
 def main():
     """ Main process """
 
     parser = ArgumentParser(description="Download bakta full database")
     parser.add_argument("-d", "--database_dir", help="directory to save database in", required=True)
+    parser.add_argument("-v", "--database_version", help="database version to download", default='latest')
+    parser.add_argument("-t", "--database_type", help="database type to download (full or light)", default='light')
     args = parser.parse_args()
 
-    database_dir = f"{args.database_dir}/bakta_db/"
+    database_dir = Path(f"{args.database_dir}/bakta_db/")
 
     try:
-        Path(database_dir).mkdir(exist_ok=True, parents=True)
+        database_dir.mkdir(exist_ok=True, parents=True)
     except FileExistsError as e:
         print(e)
 
-    db_url = "https://zenodo.org/records/14916843/files/db.tar.xz?download=1"
+    record_id, version = get_zenodo_db_version('10.5281/zenodo.4247252', 'latest')
+    bakta_url = get_zenodo_download_url(record_id, args.database_type)
 
     local_db_file = Path(f'{database_dir}/bakta.tar.xz')
 
-    download_file_parallel(db_url, local_db_file, number_of_threads=16)
+    if type == 'full':
+        download_file_parallel(bakta_url, local_db_file, number_of_threads=16)
+    else:
+        download_file(bakta_url, local_db_file)
 
-    with tarfile.open(f"{local_db_file}", "r:xz") as handle:
-        handle.extractall(path=f'{database_dir}/', filter="data") 
+    with tarfile.open(f"{local_db_file}", "r") as handle:
+        # Strip the leading directory from the member names
+        members = []
+        for member in handle.getmembers():
+            parts = member.name.split(os.sep)
+            
+            if len(parts) > 1:
+                member.name = os.path.join(*parts[1:])
+                members.append(member)
+        
+        # Extract only the modified members
+        handle.extractall(path=database_dir, members=members, filter="data")
+
+    update_db_version(args.database_dir, 'bakta', version, args.database_type)
 
     os.remove(local_db_file)
 
