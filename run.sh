8f6807b9899714653ea64cda3286d7173d709d28
James Abbott <j.abbott@dundee.ac.uk>
2026-04-13 20:44:28 +0100

    Enable customisation of apptainer cache location to enable

--- run.sh
+++ run.sh
@@ -2,8 +2,6 @@
 
 # Runs a Snakemake workflow to generate assemblies from raw data within data directory
 
-set -e 
-
 # Some sanity checking...
 # work out where we are running from, taking symlinks into account
 run_dir=$(readlink -f "$0")
@@ -23,4 +21,19 @@ if [[ "${base_dir}" != "${analysis_root}" ]]; then
 	exit 1
 fi
 
+# Set a permissive umask so that files created by Snakemake are group-writable,
+# to help with sharing databases and apptainer caches
+umask 002
+
+# Firstly pull the apptainer images to make sure they download successfully
+# allowing us to generate a sensible error message...
+echo "Pulling required apptainer images..."
+
+snakemake --use-apptainer --conda-create-envs-only --cores 1
+if [[ $? -ne 0 ]]; then
+	echo "Error pulling apptainer images. Please check your internet connection and try again."
+	exit 1
+fi
+
+echo "\nRunning Snakemake workflow..."
 snakemake --profile profile $@
